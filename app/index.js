c8370dd52e3d2faa7117325d293f99ccb8253af6
philipwaluisachi <philipwaluisachi@gmail.com>
2021-11-24 00:15:21 +0300

    app's server coded

---
+++ app/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+require(dotenv).config();
+// Modules
+const connectDB = require('../database');
+// routes
+const auth = require('../route/auth');
+const products = require('../route/products');
+const sales = require('../route/sales');
+
+// Initialization
+const app = express();
+const base_uri = 'api/v1'
+
+// Database
+connectDB();
+// Middlewares
+app.use(express.json());
+
+// =>Routes
+app.use(`${base_uri}/auth`, auth);
+app.use(`${base_uri}/products`, products);
+app.use(`${base_uri}/sales`, sales);
+
+// server
+
+const server = app.listen(process.env.PORT, () => console.log(`App running on port: ${process.env.PORT}`));
